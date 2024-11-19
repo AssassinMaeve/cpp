74b235bab1163b70974b22dc72442d14f3e663cb
AssassinMaeve <maeve.fernandes.7@gmail.com>
2024-11-19 21:32:51 +0530

    practice

--- Practice Data Structure/dqueuep.cpp
+++ Practice Data Structure/dqueuep.cpp
@@ -1,77 +1,75 @@
 #include<iostream>
 using namespace std;
 
-int dqueue[5], front=-1, rear = -1, n = 5;
+int dqueue[5],front = -1, rear = -1, n = 5;
 
 void insertfront(){
     int val;
-    if(front == -1 && rear == -1){
-        cout<<"The queue is full"<<endl;
-    }else if(front == -1 && rear == -1){
+    if(front == 0 && rear == n - 1){
+        cout<<"The Queue is full"<<endl;
+        return;
+    }else if(front == -1 && rear == - 1){
         front=rear=0;
-        cout<<"Enter the elements: "<<endl;
+        cout<<"Enter the value in front: "<<endl;
         cin>>val;
         dqueue[front]=val;
-    }else if(front = -1){
-        front = 0;
-        cout<<"Cannot enter in the front anymore"<<endl;
     }else{
-        cout<<"Enter the elements: "<<endl;
+         if(front==0){
+        cout<<"Cannot add more in the front"<<endl;
+        return;
+    }else{
+        cout<<"Enter the value in front: "<<endl;
         cin>>val;
-        dqueue[--front]=val;
+        front--;
+        dqueue[front]=val;
     }
+}
 }
 
 void insertrear(){
-    if(rear == -1){
-        cout<<"The queue is full"<<endl;
-    }else if (front == -1 && rear == -1){
+    int val;
+    if(rear == n - 1){
+        cout<<"The Queue is full"<<endl;
+    }else if(front == -1 && rear == - 1){
         front=rear=0;
-        cout<<"Enter the elements in the rear: "<<endl;
-        int val;
+        cout<<"Enter the value in rear: "<<endl;
         cin>>val;
         dqueue[rear]=val;
     }else{
-        cout<<"Enter the elements in the rear: "<<endl;
-        int val;
+        cout<<"Enter the value in rear: "<<endl;
         cin>>val;
-        dqueue[--rear]=val;
+        dqueue[++rear]=val;
     }
 }
 
 void deletefront(){
-    if (front == -1 && rear == -1){
-        cout<<"the queue is empty"<<endl;
-        return;
-    }else if(front==rear){
-        cout<<"The deleted element is "<<dqueue[front]<<endl;
-        front=rear=-1;
+    if(front == -1 && rear == -1){
+        cout<<"The Queue is empty"<<endl;
+    }else if(front == rear){
+        front == rear == -1;
     }else{
-        cout<<"The deleted element is "<<dqueue[front]<<endl;
+        cout<<"The deleted element is: "<<dqueue[front]<<endl;
         front++;
     }
 }
 
 void deleterear(){
-    if (front == -1 && rear == -1){
-        cout<<"the queue is empty"<<endl;
-        return;
-    }else if(front==rear){
-        cout<<"The deleted element is "<<dqueue[rear]<<endl;
-        front=rear=-1;
+    if(front == -1 && rear == -1){
+        cout<<"The Queue is empty"<<endl;
+    }else if(front == rear){
+        front == rear == -1;
     }else{
-        cout<<"The deleted element is "<<dqueue[rear]<<endl;
+        cout<<"The deleted element is: "<<dqueue[rear]<<endl;
         rear--;
     }
 }
 
 void display(){
-    if (front == -1 && rear == -1){
-        cout<<"the queue is empty"<<endl;
-        return;
+    if(front == -1 && rear == -1){
+        cout<<"The Queue is empty"<<endl;
     }else{
-        cout<<"The elements are: "<<endl;
-        for(int i = front ; i < rear ; i++){
+        cout<<"The element is: "<<endl;
+        for(int i=front;i<=rear;i++){
             cout<<dqueue[i]<<endl;
         }
     }
@@ -79,27 +77,39 @@ void display(){
 
 int main(){
     int ch;
-    cout << "1. Insert at the front" << endl;
-    cout << "2. Insert at the rear" << endl;
-    cout << "3. Delete from the front" << endl;
-    cout << "4. Delete from the rear" << endl;
-    cout << "5. Display" << endl;
-    cout << "6. Exit" << endl;
 
-    do {
-        cout << "Enter your choice: ";
-        cin >> ch;
+    cout<<"1.Insert in the front:"<<endl;
+    cout<<"2.Insert in the rear:"<<endl;
+    cout<<"3.Delete in the front:"<<endl;
+    cout<<"4.Delete in the rear:"<<endl;
+    cout<<"5.Display"<<endl;
+    cout<<"6.Exit"<<endl;
 
-        switch (ch) {
-            case 1: insertfront(); break;
-            case 2: insertrear(); break;
-            case 3: deletefront(); break;
-            case 4: deleterear(); break;
-            case 5: display(); break;
-            case 6: cout << "Exiting..." << endl; break;
-            default: cout << "Invalid choice" << endl; break;
+    do{
+        cout<<"Enter ur choice"<<endl;
+        cin>>ch;
+
+        switch(ch){
+            case 1:insertfront();
+            break;
+
+            case 2:insertrear();
+            break;
+
+            case 3:deletefront();
+            break;
+
+            case 4:deleterear();
+            break;
+
+            case 5:display();
+            break;
+
+            case 6:cout<<"Exiting..."<<endl;
+            break;
+
+            default:cout<<"Invalid choice"<<endl;
+            break;
         }
-    } while (ch != 6);
-
-    return 0;
+    }while(ch!=6);
 }
--- Practice Data Structure/infixtopostfixp.cpp
+++ Practice Data Structure/infixtopostfixp.cpp
@@ -1,51 +1,56 @@
-#include<iostream>
-#include<stack>
-#include<string>
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
-int prec(char op){
-    if(op=='*' || op=='/'){
-        return 2;
-    }
-    if(op=='+' || op=='-'){
+
+int prec(char op) {
+    if (op == '+' || op == '-') {
         return 1;
     }
-    if(op=='^'){
-        return 3;
-    }else{
-        return -1;
+    if (op == '*' || op == '/') {
+        return 2;
     }
+    if (op == '^') {
+        return 3;
+    }
+    return -1;
 }
 
-string infixtopostfixp(string s){
+string infixtopostfix(string s) {
     stack<char> st;
     string res;
-    for(char ch: s){
-        if(isalnum(ch)){
-            res+=ch;
-        }else{
-        while(!st.empty()&&prec(st.top())>=prec(ch)){
-            res+=st.top();
-            st.pop();
-            
+
+    for (size_t i = 0; i < s.length(); i++) {
+        char ch = s[i];
+
+        // If the character is an operand, add it to the result
+        if (isalnum(ch)) {
+            res += ch;
         }
-        st.push(ch);
+        // If the character is an operator
+        else {
+            while (!st.empty() && prec(st.top()) >= prec(ch)) {
+                res += st.top();
+                st.pop();
+            }
+            st.push(ch);
+        }
     }
-}
-    while(!st.empty()){
-        res+=st.top();
+
+    // Pop all the remaining operators from the stack
+    while (!st.empty()) {
+        res += st.top();
         st.pop();
-    }   
+    }
+
     return res;
 }
 
+int main() {
+    string s;
+    cout << "Enter an infix expression: " << endl;
+    cin >> s;
 
-
-int main(){
-    string ch;
-
-    cout<<"Enter an infix "<<endl;
-    cin>>ch;
-
-    cout<<"The postfix expression is "<<infixtopostfixp(ch)<<endl;
+    cout << "Postfix expression: " << infixtopostfix(s) << endl;
     return 0;
 }
