c6482d8b06a8fcc5e594d2b4bab5cb07ef6dae3f
AssassinMaeve <maeve.fernandes.7@gmail.com>
2024-11-24 22:19:03 +0530

    Data Structure

--- Data Structure Codes/sparcematrix.cpp
+++ Data Structure Codes/sparcematrix.cpp
@@ -1,42 +1,74 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct element{
+
+struct element {
     int row;
     int col;
     int value;
 };
 
-class spasematrix{
-    private:
-            element element[10];
-            int count;
-    
-    public:
-            spasematrix():count(0){}
+class spasematrix {
+private:
+    element elements[10]; // Fixed array to store sparse matrix elements
+    int count;            // Number of non-zero elements
 
-            void insert(int row, int col, int value){
-                if(value!=0 && count<10){
-                    element[count++]={row,col,value};
+public:
+    spasematrix() : count(0) {} // Constructor initializes count to 0
 
-                }
+    void insert(int row, int col, int value) {
+        if (value != 0 && count < 10) {
+            elements[count++] = {row, col, value}; // Add non-zero element
+        } else if (value == 0) {
+            cout << "Zero values are not stored in a sparse matrix!" << endl;
+        } else {
+            cout << "Matrix is full, cannot add more elements!" << endl;
+        }
+    }
+
+    void display() {
+        if (count == 0) {
+            cout << "Sparse matrix is empty!" << endl;
+        } else {
+            cout << "Sparse Matrix Elements:\n";
+            for (int i = 0; i < count; i++) {
+                cout << " Row " << elements[i].row
+                     << " Col " << elements[i].col
+                     << " Value " << elements[i].value << endl;
             }
-
-            void display(){
-                for (int i=0;i<count;i++){
-                    cout<<" Row "<< element[i].row 
-                        <<" Col "<< element[i].col 
-                        <<" Value "<< element[i].value <<endl;
-                }
-            }
-
+        }
+    }
 };
 
-int main(){
+int main() {
     spasematrix s;
-    s.insert(0,0,5);
-    s.insert(0,0,1);
-    s.insert(3,0,2);
-    s.display();
-    return 0;
+    int choice;
 
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Insert Element\n";
+        cout << "2. Display Sparse Matrix\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                int row, col, value;
+                cout << "Enter row, column, and value (row col value): ";
+                cin >> row >> col >> value;
+                s.insert(row, col, value);
+                break;
+            }
+            case 2:
+                s.display();
+                break;
+            case 3:
+                cout << "Exiting program..." << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+    } while (choice != 3);
+
+    return 0;
 }
