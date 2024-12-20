1e0f687e43fae182db32a10c8f8d96ebf7912cbb
AssassinMaeve <maeve.fernandes.7@gmail.com>
2024-12-20 21:38:36 +0530

    Linked List

--- Data Structure Codes/Linked List/stackUsingLinkedList.cpp
+++ Data Structure Codes/Linked List/stackUsingLinkedList.cpp
@@ -1,105 +1,105 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node
-{
-    int val;                 // Stores data
-    Node* next;             // Pointer to next node
+// Node structure for linked list
+struct Node {
+    int val;        // Stores data
+    Node* next;    // Pointer to next node
 };
 
-    
-class mystack{
+// Stack class using linked list
+class mystack {
+    Node* head;       // Pointer to the top of the stack
+    int stacksize;    // Number of elements in the stack
 
-    Node* head;             // Pointer to the top of the stack
-    int stacksize;          // Number of elements in the stack
-
-        public:             // The constructor mystack() should be public so that you can create instances of mystack in main()
-    mystack(){
-
+public:
+    // Constructor to initialize stack
+    mystack() {
         head = NULL;
         stacksize = 0;
-
     }
 
-    // This operation inserts the node at head;
-    void Push(int g){
+    // Push operation to insert a node at head
+    void Push(int g) {
+        Node* temp = new Node();  // Create a new node
+        temp->val = g;            // Assign value to the node
+        temp->next = head;        // Point new node's next to current head
+        head = temp;              // Update head to new node
 
-        Node* temp = new Node();        // Create a new node
-        temp -> val = g;                // Assign value to the node
-        temp -> next = head;            // Point new node's next to current head
-        head = temp;                    // Update head to new node
-
-        cout<<"Element "<<g<<" has been pushed to stack :)"<<endl;
+        cout << "Element " << g << " has been pushed to stack :)" << endl;
         stacksize++;
-
     }
 
-    // This operation deletes the node from head
-    void Pop(){
-
-        if(head==NULL){
-            cout<<"Stack is empty cannot Pop :("<<endl;
+    // Pop operation to delete the node from head
+    void Pop() {
+        if (head == NULL) {
+            cout << "Stack is empty, cannot Pop :(" << endl;
             return;
         }
 
-
-        Node* temp = head;              // Store current head
-        head = temp -> next;            // Move head to next node
-        temp -> next = NULL;            // Delete old head
-        delete temp;
-        cout << "The element has be Popped :) " << endl;
+        Node* temp = head;          // Store current head
+        head = temp->next;          // Move head to next node
+        delete temp;                // Delete old head
+        cout << "The element has been Popped :)" << endl;
         stacksize--;
     }
 
-    // displays the top element in the stack
-    void top(){
-        if ( head == NULL ){
+    // Display the top element in the stack
+    void top() {
+        if (head == NULL) {
             cout << "No top element is present. The Stack is Empty! :( " << endl;
-        }else {
-            
+        } else {
             cout << "The Element at top is: " << head->val << endl;
-
         }
-
-        
-
     }
 
-    // this operation check the stack size
-    void size(){
+    // Check the size of the stack
+    void size() {
         cout << "The size of the stack is: " << stacksize << endl;
     }
 
-    void empty(){
-
-        if ( head == NULL){
+    // Check if the stack is empty
+    void empty() {
+        if (head == NULL) {
             cout << "The stack is empty! :( " << endl;
-            
         } else {
             cout << "The stack is not empty! " << endl;
         }
-
     }
 
-
-
+    // Display all elements in the stack
+    void display() {
+        if (head == NULL) {
+            cout << "Stack is empty" << endl; // If stack is empty, display message
+            return;
+        }
+        
+        Node* temp = head;  // Start from the top of the stack
+        cout << "Stack elements are: ";
+        
+        while (temp != NULL) {  // Traverse and print each element
+            cout << temp->val << " ";  // Use 'val' instead of 'data'
+            temp = temp->next;
+        }
+        
+        cout << endl;
+    }
 };
 
-int main(){
-
+int main() {
     mystack s;          // Create an instance of mystack
 
     int choice;  // Variable for user choice
     int value;   // Variable for value input
 
-    
-        cout << "<-------Stack Operation------->" << endl;
-        cout << "1. Push" << endl;
-        cout << "2. Pop" << endl;
-        cout << "3. Display" << endl;
-        cout << "4. Check Size" << endl;
-        cout << "5. Check if empty" << endl;
-        cout << "6. Exit" << endl;
+    cout << "<-------Stack Operation------->" << endl;
+    cout << "1. Push" << endl;
+    cout << "2. Pop" << endl;
+    cout << "3. Display Top Element" << endl;
+    cout << "4. Check Size" << endl;
+    cout << "5. Check if Empty" << endl;
+    cout << "6. Exit" << endl;
+    cout << "7. Display All Elements" << endl;
 
     do {
         cout << "Enter your choice: ";  // Prompt user for choice
@@ -117,7 +117,7 @@ int main(){
                 break;
 
             case 3:
-                s.top();            // Call display function to show all elements
+                s.top();                // Display top element in the stack
                 break;
 
             case 4:
@@ -130,6 +130,10 @@ int main(){
 
             case 6:
                 cout << "Exiting....." << endl; // Exit message
+                break;
+
+            case 7:
+                s.display();            // Display all elements in the stack
                 break;
 
             default:
