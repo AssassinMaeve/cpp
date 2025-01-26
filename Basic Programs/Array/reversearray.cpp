7afe1eea2235d9f10bb27dfbcbc81e7f34530892
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-01-26 12:10:20 +0530

    Basic Programs

--- Basic Programs/Array/reversearray.cpp
+++ Basic Programs/Array/reversearray.cpp
@@ -1,36 +1,3 @@
-/*
-#include<iostream>
-using namespace std;
-
-/*
-void print(int *arr, int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-}
-
-
-void reverse(int *arr, int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[n-1 -i];
-    }
-}
-
-int main(){
-    int n;
-    cin>>n;
-
-    int arr[n];
-
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-        reverse(arr,n);
-    }
-    
-
-    return 0;
-}
-*/
 #include<iostream>
 using namespace std;
 
