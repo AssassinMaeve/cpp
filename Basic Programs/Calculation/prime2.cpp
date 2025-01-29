878b863d90eba4fe8c8385b26efaa9062a0b54fe
AssassinMaeve <maeve.fernandes.7@gmail.com>
2025-01-29 13:46:50 +0530

    Basic Programs

--- Basic Programs/Calculation/prime2.cpp
+++ Basic Programs/Calculation/prime2.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <cmath> // Include the cmath library for sqrt function
 using namespace std;
-int main(){
-    int n,i;
-    cout<<"Enter the Value"<<endl;
-    cin>>n;
 
-    for(i=2;i<n;i++){
-        if(n%i==0){
-        cout<<"Not a Prime Number"<<endl;
-        break;
+bool isPrimeOptimised(int n){
+    if (n <= 1) return false; // Handle edge cases
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0){
+            return false;
         }
     }
+    return true;
+}
 
-    if(i==n){
-        cout<<"Is a Prime Number"<<endl;
-    }else{
-        cout<<"Not a Prime Number"<<endl;
+int main(){
+    int n;
+    cout << "Enter the Value" << endl;
+    cin >> n;
+
+    if (isPrimeOptimised(n)) {
+        cout << "Is a Prime Number" << endl;
+    } else {
+        cout << "Not a Prime Number" << endl;
     }
     return 0;
 }
